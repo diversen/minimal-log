1510fe79a0c8f2ea8b8dc58e713d664d4c201594
Dennis Iversen <dennis.iversen@gmail.com>
2018-12-14 15:15:09 +0100

    fomatting

--- log.php
+++ log.php
@@ -3,17 +3,18 @@
 namespace diversen;
 
 /**
- * Class log contains methods for doing 
+ * Class log contains methods for doing
  * logging
  */
-class log {
+class log
+{
 
     /**
-		 * Logfile
+     * Logfile
      * @var string $logfile
      */
     public static $logFile = 'error.log';
-    
+
     /**
      * Debug flag
      * var boolean $debug default is false
@@ -23,18 +24,20 @@ class log {
     /**
      * Enable debug flags. Set high error level and output any error and debug notices
      */
-    public static function enableDebug () {
+    public static function enableDebug()
+    {
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
         self::$debug = true;
     }
-    
+
     /**
      * Logs an error. If debug is set then output to console or browser
      * @param string $message
      */
-    public static function error ($message) {
+    public static function error($message)
+    {
 
         if (!is_string($message)) {
             $message = var_export($message, true);
@@ -43,49 +46,51 @@ class log {
         if (!self::isCli()) {
             $output_message = "<pre>" . $message . "</pre>";
         } else {
-						$output_message = $message;
-				}
+            $output_message = $message;
+        }
 
         if (self::$logFile) {
             error_log($message . PHP_EOL, 3, self::$logFile);
         }
 
-				if (self::$debug) {
-						echo $output_message . PHP_EOL;
-				}
+        if (self::$debug) {
+            echo $output_message . PHP_EOL;
+        }
     }
-    
+
     /**
      * Checks if we are in CLI mode
      * @return boolean $res true if cli else false
      */
-    public static function isCli () {
-        
+    public static function isCli()
+    {
+
         if (php_sapi_name() === 'cli') {
             return true;
         }
         return false;
     }
-    
+
     /**
-     * Debug a message. Writes to stdout and to log file 
+     * Debug a message. Writes to stdout and to log file
      * if debug = false then any message to this method is ignored
-     * @param string $message 
+     * @param string $message
      */
-    public static function debug ($message) {       
+    public static function debug($message)
+    {
         if (self::$debug) {
             self::error($message);
             return;
-        } 
+        }
     }
 
     /**
-     * Set error log file. 
+     * Set error log file.
      * Is used for CLI - as CLI does not have a default log file
      * @param string $file
-     */    
-    public static function setErrorLogFile($file) {
+     */
+    public static function setErrorLogFile($file)
+    {
         self::$logFile = $file;
     }
 }
-
